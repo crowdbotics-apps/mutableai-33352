49548022109111d4f97ebe683ded636c829a08a1
CloudEnochCSIS <155973884+CloudEnochCSIS@users.noreply.github.com>
2024-12-12 20:50:01 +0000

    Update startup.sh

--- backend/scripts/startup.sh
+++ backend/scripts/startup.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --no-input
 python manage.py migrate --noinput
 
-waitress-serve --port=$PORT sweet_scene_37828.wsgi:application
+waitress-serve --port=$PORT mutableai_33352.wsgi:application
